e1eab2950c3af778f6dd7ebbe5b8b841d76767ef
K-waks <72149672+K-waks@users.noreply.github.com>
2023-01-03 03:09:39 +0300

    First commit of project files

---
+++ patients/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+
+
+class Patient(models.Model):
+    first_name = models.CharField(max_length=200)
+    surname = models.CharField(max_length=200)
+    age = models.SmallIntegerField()
+    sex = models.CharField(max_length=50)
+    cp = models.CharField(max_length=50)
+    trestbps = models.SmallIntegerField()
+    chol = models.SmallIntegerField()
+    fbs = models.CharField(max_length=50)
+    restecg = models.CharField(max_length=50)
+    thalach = models.SmallIntegerField()
+    exang = models.CharField(max_length=50)
+    oldpeak = models.FloatField()
+    slope = models.CharField(max_length=50)
+    ca = models.SmallIntegerField()
+    thal = models.CharField(max_length=50)
+    target = models.CharField(max_length=50)
+
+    def _str__(self):
+        return self.first_name
